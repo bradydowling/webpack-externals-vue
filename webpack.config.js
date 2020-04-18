90d680ff408f2838a250f1b69729c7aac633dcb1
bradydowling <bradydowling@gmail.com>
2020-04-18 08:21:54 -0400

    Working

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+};
